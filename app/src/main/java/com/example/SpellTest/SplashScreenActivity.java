5387141088f07fc3466ff2524423878ae0df97f6
Steve Granger <grangsj@gmail.com>
2019-03-19 20:30:39 -0400

    With initial icons and wireframe splash screen.  Need to update with better graphics.

--- app/src/main/java/com/example/SpellTest/SplashScreenActivity.java
+++ app/src/main/java/com/example/SpellTest/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.example.SpellTest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,20 +11,42 @@ import com.example.myapplication.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    //Timer variables - used to shut down splash screen when done.
+    private Handler handler = new Handler();
+    private Runnable timer = new Runnable(){
+        @Override
+        //When fired, this Runnable fires an intent to show the next screen and kill this activity.
+        public void run() {
+            Intent i = new Intent(SplashScreenActivity.this, UserSelectionActivity.class);
+            startActivity(i);
+            finish();    //Prevents user from coming back to this screen via back button
+        }
+    };
+
+    private static final int SPLASH_SCREEN_DURATION = 1000;  //Duration of splash screen (in milliseconds).
+
+    //Called when Splash screen is first displayed.  Note that the splash screen is built as a theme, so
+    //we don't need a layout here - just going to show the theme.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        Button forwardButton = this.findViewById(R.id.splash_button_user_selection);
-        forwardButton.setOnClickListener(new View.OnClickListener(){
+        //Wait for defined time, then issue a Runnable to shut down the splash screen.
+        handler.postDelayed(timer, SPLASH_SCREEN_DURATION);
+    }
 
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(SplashScreenActivity.this, UserSelectionActivity.class);
-                startActivity(i);
-                finish();    //Prevents user from coming back to this screen via back button
-            }
-        });
+
+
+    /**
+     * Called when user leaves splash screen.  Added this method to shut down the Runnable if the user hits the
+     * back button while the splash screen is on - otherwise the Runnable will show the next screen even though
+     * the user asked to leave program.
+     */
+    @Override
+    protected void onPause() {
+
+        //Removes any Runnable instances in the Handler (ie, if the timer is still running!)
+        handler.removeCallbacks(timer);
+        super.onPause();
     }
 }
