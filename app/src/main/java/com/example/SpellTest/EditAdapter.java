5362f2f5713852ae0c88b6641916878b23427e30
Steve Granger <grangsj@gmail.com>
2019-04-07 20:39:08 -0400

    Started commenting the code.

--- app/src/main/java/com/example/SpellTest/EditAdapter.java
+++ app/src/main/java/com/example/SpellTest/EditAdapter.java
@@ -1,3 +1,16 @@
+/**
+ * Filename:  EditAdapter.java
+ * Author:  Team SpellTest
+ * Date:  07 April 2019
+ *
+ * Purpose:  This class represents the backing adapter for the Word List Builder activity.  This class
+ * creates and populates each row of data in the Word List Builder, and also provides the "business logic"
+ * for the buttons shown in each of the list items.
+ *
+ * Note that, given the (relative) complexity of this class, we broke this out into a separate class
+ * rather than a nested class.  This should make the code a little easier to read and follow.
+ */
+
 package com.example.spelltest;
 
 import android.content.Context;
@@ -9,35 +22,65 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
-
 import com.example.myapplication.R;
-
 import java.util.ArrayList;
 
 public class EditAdapter extends RecyclerView.Adapter {
 
-    private ArrayList<Objects.Word> mData;
-    private Context mContext;
-    private DataStore mDataStore;
+    //Instance variables
+    private ArrayList<Objects.Word> mData;      //The backing data for this adapter (stored locally)
+    private Context mContext;                   //The Context to which this adapter is attached (typically from the parent Activity).
+    private DataStore mDataStore;               //A reference to the application data store.
 
+    /**
+     * Class constructor.  This overrides the parent constructor by adding some additional useful
+     * fields for this class.
+     *
+     * @param context the Context object to which this adapter is associated (should be a WordListBuilderActivity).
+     * @param data the initial backing array of words displayed by this adapter.
+     */
     public EditAdapter (Context context, ArrayList<Objects.Word> data){
 
+        //Run the super constructor.
         super();
+
+        //Save the incoming values for later use in the class.
         mData = data;
         mContext = context;
+
+        //Create and save a link to the application datastore.
         mDataStore = DataStore.newInstance(context);
     }
 
+    /**
+     * Method called when a specific list item needs to be shown on the screen.  We will use this
+     * method to inflate the item view only - populating it with data is done later (in onBindViewHolder).
+     *
+     * @param parent the parent RecyclerView to which this list item will be attached.
+     * @param i  the element in the backing array represented by this specific list item.
+     * @return the ListViewHolder item for the desired item in the list.
+     */
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
 
+        //Inflate the layout for this list item.
         View newView = LayoutInflater.from(mContext).inflate(R.layout.item_edit_list, parent, false);
+
+        //Instantiate an EditViewHolder with this new view.
         EditViewHolder vh = new EditViewHolder(newView);
 
+        //Return the EditViewHolder we just instantiated.
         return vh;
     }
 
+    /**
+     * Public method to bind an inflated ViewHolder to its backing data.  This is where we update the
+     * text field in each item, and we also wire up the listeners in the list item here as well.
+     *
+     * @param viewHolder the EditViewHolder for which data needs to be added.
+     * @param i The specific item in the backing array from which the text in this ViewHolder needs to be updated.
+     */
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, final int i) {
 
@@ -51,13 +94,18 @@ public class EditAdapter extends RecyclerView.Adapter {
         vh.mInputBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
+
+                //If we just lost focus...
                 if (!hasFocus){
 
+                    //Obtain the current word in the adapter array, and change the spelling to match whatever the user typed in.
                     Objects.Word currentWord = mData.get(i);
                     currentWord.spelling = vh.mInputBox.getText().toString();
 
+                    //Update the application datastore with the revised word.
                     mDataStore.putWord(currentWord);
 
+                    //Notify the adapter that this item has changed, so it can show it properly.
                     notifyItemChanged(i);
                 }
             }
@@ -67,11 +115,17 @@ public class EditAdapter extends RecyclerView.Adapter {
         vh.mDeleteImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Obtain the Word object for the current word.
                 Objects.Word currentWord = mData.get(i);
+
+                //Delete the word from the application datastore...
                 mDataStore.deleteWord(currentWord.id);
+
+                //...then delete it from the adapter backing array.
                 mData.remove(i);
 
-
+                //Notify the adapter that the item has been removed, and that the range of data displayed has changed.
                 notifyItemRemoved(i);
                 notifyItemRangeChanged(i, mData.size());
             }
@@ -79,25 +133,57 @@ public class EditAdapter extends RecyclerView.Adapter {
 
     }
 
+    /**
+     * Method to return a count for the total number of items in the array.  Android requires this
+     * method to be overridden for derived classes of ListAdapter.
+     *
+     * @return the total number of items in the array.
+     */
     @Override
     public int getItemCount() {
+
+        //Just return the size of the backing array.
         return mData.size();
     }
 
+    /**
+     * Public method to add a new word to the adapter and to the datastore.  We call this from the parent Activity
+     * whenever the user presses the Add New Word button (which is associated with the activity).
+     * @param word the new Word object to be added to the backing array.
+     */
     public void addWord(Objects.Word word){
+
+        //Save the word to the application datastore, and reset the (empty) word id to be the assigned id.
+        long id = mDataStore.putWord(word);
+        word.id = id;
+
+        //Add the word to the adapter backing array as well.
         mData.add(word);
+
+        //Notify the adapter that a new item has been added.
         notifyItemInserted(mData.size()-1);
     }
 
-
+    /**
+     * Private class that represents the view structure of each element.  This
+     * is a pretty dumb class - the only thing it does is save references to the
+     * child views in the list item, so that they can be referenced in the adapter.
+     */
     private class EditViewHolder extends RecyclerView.ViewHolder {
 
-        public EditText mInputBox;
-        public ImageView mDeleteImage;
+        //Instance variables (one of these exists per list item)
+        public EditText mInputBox;          //Link to the input edit text box.
+        public ImageView mDeleteImage;      //Link to the delete button.
 
+        /**
+         * Constructor for the view holder.  Teis is where we'll collect and
+         * save references to the child list items (so the adapter can use them later).
+         * @param itemView  the inflated view for this ViewHolder.
+         */
         public EditViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            //Save references to the child view items, so the adapter can use them later.
             mInputBox = itemView.findViewById(R.id.edit_list_item_text);
             mDeleteImage = itemView.findViewById(R.id.edit_list_item_delete);
         }
--- app/src/main/java/com/example/spelltest/WordListBuilderActivity.java
+++ app/src/main/java/com/example/spelltest/WordListBuilderActivity.java
@@ -31,53 +31,61 @@ import java.util.ArrayList;
 
 public class WordListBuilderActivity extends AppCompatActivity {
 
-    public static final String EXTRA_LIST_ID = "com.example.spelltest.list_id";
-    private static final String EMPTY_STRING="";
-    private Handler handler = new Handler();
+    //Class variables
+    public static final String EXTRA_LIST_ID = "com.example.spelltest.list_id"; //Tag for Extra transmission (ie identification of spelling list)
+    private static final String EMPTY_STRING="";                                //Empty string
 
-    private long mListId = DataStore.NULL_ROW_ID;
-    private static final String TAG = "WordListBuilderActivity";
-    private RecyclerView mRecyclerView;
-    private EditAdapter mAdapter;
-    private DataStore mData;
+    //Instance variables
+    private long mListId = DataStore.NULL_ROW_ID;                               //id of the Spelling List we're editing
+    private static final String TAG = "WordListBuilderActivity";                //Tag to identify class during debugging
+    private RecyclerView mRecyclerView;                                         //Reference to the RecyclerView for this activity
+    private EditAdapter mAdapter;                                               //Data adapter for the RecyclerView
+    private DataStore mData;                                                    //Reference to application datastore
 
+    /**
+     * Method called by Android when the activity is first created.  We use this to inflate the layout
+     * for the activity and wire up most of the views and buttons.
+     * @param savedInstanceState a Bundle with saved state information (not used in this app).
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Call the super constructor
         super.onCreate(savedInstanceState);
+
+        //Inflate the layout for the activity, and set the activity's view to this inflated layout.
         setContentView(R.layout.activity_word_list_builder);
 
+        //Retain the link to the RecyclerView, and set the layout manager for ut.
         mRecyclerView = (RecyclerView) findViewById(R.id.word_list_builder_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //Get the list id (send from the ListSelectionActivity class)
+        //Get the list id (send from the ListSelectionActivity class), then get the list of spelling words from the DataStore
         mListId = getIntent().getLongExtra(EXTRA_LIST_ID, DataStore.NULL_ROW_ID);
-
         mData = DataStore.newInstance(this);
         ArrayList<Objects.Word> wordList = mData.getWords(mListId);
 
+        //Instantiate the adapter for the RecyclerView, and link it to the REcyclerView
         mAdapter = new EditAdapter(this, wordList);
         mRecyclerView.setAdapter(mAdapter);
 
+        //Wire up the "add word" button
         FloatingActionButton fab = findViewById(R.id.word_add_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Create a new blank word object
                 Objects.Word word = new Objects.Word(
                         DataStore.NULL_ROW_ID,
                         mListId,
                         EMPTY_STRING
                 );
 
-                DataStore data = DataStore.newInstance(WordListBuilderActivity.this);
-                long id = data.putWord(word);
-                word.id = id;
-
+                //Add the new word (with the correct id) to the datastore and to the adapter as well, so it can be shown to the user.
                 mAdapter.addWord(word);
             }
         });
-
-
-
 
     }
 }
